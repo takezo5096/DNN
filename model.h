fdc840540af320c5d633923b9db993abd41a9142
takeshi.fujita <takeshi.fujita@excite.jp>
2017-01-30 21:43:56 +0900

    add conv2d and pooling

--- model.h
+++ model.h
@@ -20,8 +20,9 @@
 #include <boost/archive/binary_oarchive.hpp>
 #include <boost/archive/binary_iarchive.hpp>
 
-#include "function.h"
+//#include "function.h"
 #include "variable.h"
+#include "graph.h"
 
 using namespace std;
 
@@ -38,14 +39,15 @@ public:
 class Model {
 private:
 
+
     friend class boost::serialization::access;
     template<class Archive> void serialize(Archive & ar, const unsigned int version) {
-        ar & funcs;
+        ar & graphs;
     }
 
 
 public:
-    map<string, Function *> funcs;
+    map<string, Graph *> graphs;
     vector<UpdateParams *> updateParams;
 
     ~Model(){
@@ -54,176 +56,157 @@ public:
         }
     }
 
-    void putF(string name, Function *f){
-        funcs[name] = f;
+    void putG(string name, Graph *f){
+        graphs[name] = f;
     }
-    Function *f(string name){
-        return funcs.at(name);
+    Graph *G(string name){
+        return graphs.at(name);
     }
 
     vector<UpdateParams *> &getUpdateParams(){
-        for(auto fs : funcs){
-            Function *f = fs.second;
+        for(auto gs : graphs){
+            Graph *g = gs.second;
 
-            const type_info& id = typeid(*f);
+            const type_info& id = typeid(*g);
 
-            if (typeid(FunctionLinear) == id){
+            if (typeid(Linear) == id){
                 UpdateParams *p = new UpdateParams();
-                Variable *w = &((FunctionLinear *)f)->w;
+                Variable *w = ((Linear *)g)->w;
                 p->add(w);
-                if (!((FunctionLinear *)f)->noBias){
-                    Variable *b = &((FunctionLinear *)f)->b;
+                if (!((Linear *)g)->noBias){
+                    Variable *b = ((Linear *)g)->b;
                     p->add(b);
                 }
                 updateParams.push_back(p);
-            }
-            else if (typeid(FunctionEmbed) == id) {
+
+            } else if (typeid(LSTM) == id){
+
                 UpdateParams *p = new UpdateParams();
-                Variable *w = &((FunctionEmbed *) f)->w;
-                p->add(w);
-                if (!((FunctionEmbed *) f)->noBias) {
-                    Variable *b = &((FunctionEmbed *) f)->b;
-                    p->add(b);
-                }
+                LSTM *lstm = (LSTM *)g;
+
+                p->add(lstm->x_w);
+                p->add(lstm->x_b);
+                p->add(lstm->h_w);
+                p->add(lstm->h_b);
+
+                updateParams.push_back(p);
+
+            } else if (typeid(FullLSTM) == id) {
+
+                UpdateParams *p = new UpdateParams();
+                FullLSTM *lstm = (FullLSTM *) g;
+
+                p->add(lstm->f_c_w);
+                p->add(lstm->f_h_w);
+                p->add(lstm->f_x_w);
+                p->add(lstm->f_x_b);
+                p->add(lstm->i_c_w);
+                p->add(lstm->i_h_w);
+                p->add(lstm->i_x_w);
+                p->add(lstm->i_x_b);
+                p->add(lstm->o_c_w);
+                p->add(lstm->o_h_w);
+                p->add(lstm->o_x_w);
+                p->add(lstm->o_x_b);
+                p->add(lstm->g_h_w);
+                p->add(lstm->g_x_w);
+                p->add(lstm->g_x_b);
+
+                updateParams.push_back(p);
+
+            }else if (typeid(FullLSTM2) == id){
+
+                UpdateParams *p = new UpdateParams();
+                FullLSTM2 *lstm = (FullLSTM2 *)g;
+
+                p->add(lstm->f_c_w); p->add(lstm->f_h_w); p->add(lstm->f_x_w); p->add(lstm->f_x_b);
+                p->add(lstm->i_c_w); p->add(lstm->i_h_w); p->add(lstm->i_x_w); p->add(lstm->i_x_b);
+                p->add(lstm->o_c_w); p->add(lstm->o_h_w); p->add(lstm->o_x_w); p->add(lstm->o_x_b);
+                p->add(lstm->g_h_w); p->add(lstm->g_x_w); p->add(lstm->g_x_b);
+
+                p->add(lstm->gamma_f); p->add(lstm->beta_f);
+                p->add(lstm->gamma_i); p->add(lstm->beta_i);
+                p->add(lstm->gamma_g); p->add(lstm->beta_g);
+                p->add(lstm->gamma_o); p->add(lstm->beta_o);
+
+                updateParams.push_back(p);
+
+            } else if (typeid(GRU) == id) {
+
+                UpdateParams *p = new UpdateParams();
+                GRU *lstm = (GRU *) g;
+
+                p->add(lstm->u_g); p->add(lstm->u_r); p->add(lstm->u_z);
+                p->add(lstm->w_g); p->add(lstm->w_r); p->add(lstm->w_z);
+                p->add(lstm->b_g); p->add(lstm->b_r); p->add(lstm->b_z);
+
+                updateParams.push_back(p);
+            } else if (typeid(BatchNorm) == id){
+                UpdateParams *p = new UpdateParams();
+                BatchNorm *batch_norm = (BatchNorm *) g;
+
+                p->add(batch_norm->gamma); p->add(batch_norm->beta);
+
+                updateParams.push_back(p);
+            } else if (typeid(Conv2D) == id){
+                UpdateParams *p = new UpdateParams();
+                Conv2D *conv2d = (Conv2D *) g;
+
+                p->add(conv2d->w); p->add(conv2d->b);
+
                 updateParams.push_back(p);
             }
-            else if (typeid(FunctionGRU) == id){
-                FunctionGRU *fg =  (FunctionGRU *)f;
 
-                FunctionLinear *u_h = (FunctionLinear *)fg->u_h;
-                UpdateParams *p1 = new UpdateParams();
-                p1->add(&u_h->w); if (!u_h->noBias) p1->add(&u_h->b);
-                updateParams.push_back(p1);
 
-                FunctionLinear *ur_h = (FunctionLinear *)fg->ur_h;
-                UpdateParams *p2 = new UpdateParams();
-                p2->add(&ur_h->w); if (!ur_h->noBias) p2->add(&ur_h->b);
-                updateParams.push_back(p2);
-
-                FunctionLinear *uz_h = (FunctionLinear *)fg->uz_h;
-                UpdateParams *p3 = new UpdateParams();
-                p3->add(&uz_h->w); if (!uz_h->noBias) p3->add(&uz_h->b);
-                updateParams.push_back(p3);
-
-                FunctionLinear *w_x = (FunctionLinear *)fg->w_x;
-                UpdateParams *p4 = new UpdateParams();
-                p4->add(&w_x->w); if (!w_x->noBias) p4->add(&w_x->b);
-                updateParams.push_back(p4);
-
-                FunctionLinear *wr_x = (FunctionLinear *)fg->wr_x;
-                UpdateParams *p5 = new UpdateParams();
-                p5->add(&wr_x->w); if (!wr_x->noBias) p5->add(&wr_x->b);
-                updateParams.push_back(p4);
-
-                FunctionLinear *wz_x = (FunctionLinear *)fg->wz_x;
-                UpdateParams *p6 = new UpdateParams();
-                p6->add(&wz_x->w); if (!wz_x->noBias) p6->add(&wz_x->b);
-                updateParams.push_back(p6);
-
-            }
-            else if (typeid(FunctionLSTM) == id){
-                FunctionLSTM *fl =  (FunctionLSTM *)f;
-
-                FunctionLinear *x_i = (FunctionLinear *)fl->x_i;
-                UpdateParams *p1 = new UpdateParams();
-                p1->add(&x_i->w); if (!x_i->noBias) p1->add(&x_i->b);
-                updateParams.push_back(p1);
-                FunctionLinear *w_i = (FunctionLinear *)fl->w_i;
-                UpdateParams *p2 = new UpdateParams();
-                p2->add(&w_i->w); if (!w_i->noBias) p2->add(&w_i->b);
-                updateParams.push_back(p2);
-
-                FunctionLinear *x_f = (FunctionLinear *)fl->x_f;
-                UpdateParams *p3 = new UpdateParams();
-                p3->add(&x_f->w); if (!x_f->noBias) p3->add(&x_f->b);
-                updateParams.push_back(p3);
-                FunctionLinear *w_f = (FunctionLinear *)fl->w_f;
-                UpdateParams *p4 = new UpdateParams();
-                p4->add(&w_f->w); if (!w_f->noBias) p4->add(&w_f->b);
-                updateParams.push_back(p4);
-
-                FunctionLinear *x_o = (FunctionLinear *)fl->x_o;
-                UpdateParams *p5 = new UpdateParams();
-                p5->add(&x_o->w); if (!x_o->noBias) p5->add(&x_o->b);
-                updateParams.push_back(p5);
-                FunctionLinear *w_o = (FunctionLinear *)fl->w_o;
-                UpdateParams *p6 = new UpdateParams();
-                p6->add(&w_o->w); if (!w_o->noBias) p6->add(&w_o->b);
-                updateParams.push_back(p6);
-
-                FunctionLinear *x_g = (FunctionLinear *)fl->x_g;
-                UpdateParams *p7 = new UpdateParams();
-                p7->add(&x_g->w); if (!x_g->noBias) p7->add(&x_g->b);
-                updateParams.push_back(p7);
-                FunctionLinear *w_g = (FunctionLinear *)fl->w_g;
-                UpdateParams *p8 = new UpdateParams();
-                p8->add(&w_g->w); if (!w_g->noBias) p8->add(&w_g->b);
-                updateParams.push_back(p8);
-
-            }
         }
+        //cout << " Model getUpdateParams size:" << updateParams.size() << endl;
         return updateParams;
     }
 
 
     void save(string path){
-        for(auto fs : funcs){
-            Function *f = fs.second;
+        for(auto gs : graphs){
+            Graph *g = gs.second;
 
-            const type_info& id = typeid(*f);
+            const type_info& id = typeid(*g);
 
-            if (typeid(FunctionLinear) == id){
-                ((FunctionLinear *)f)->toHostArray();
+            if (typeid(Linear) == id){
+                ((Linear *)g)->toHostArray();
+            } else if (typeid(LSTM) == id){
+                ((LSTM *)g)->toHostArray();
+            } else if (typeid(FullLSTM) == id) {
+                ((FullLSTM *) g)->toHostArray();
+            }else if (typeid(FullLSTM2) == id){
+                ((FullLSTM2 *)g)->toHostArray();
+            } else if (typeid(GRU) == id){
+                ((GRU *)g)->toHostArray();
+            } else if (typeid(BatchNorm) == id){
+                ((BatchNorm *)g)->toHostArray();
+            } else if (typeid(Conv2D) == id){
+                ((Conv2D *)g)->toHostArray();
             }
-            else if (typeid(FunctionEmbed) == id){
-                ((FunctionEmbed *)f)->toHostArray();
-            }
-            else if (typeid(FunctionGRU) == id){
-                FunctionGRU *fg =  (FunctionGRU *)f;
-                FunctionLinear *u_h = (FunctionLinear *)fg->u_h;
-                FunctionLinear *ur_h = (FunctionLinear *)fg->ur_h;
-                FunctionLinear *uz_h = (FunctionLinear *)fg->uz_h;
-                FunctionLinear *w_x = (FunctionLinear *)fg->w_x;
-                FunctionLinear *wr_x = (FunctionLinear *)fg->wr_x;
-                FunctionLinear *wz_x = (FunctionLinear *)fg->wz_x;
-                u_h->toHostArray();
-                ur_h->toHostArray();
-                uz_h->toHostArray();
-                w_x->toHostArray();
-                wr_x->toHostArray();
-                wz_x->toHostArray();
-            }
-            else if (typeid(FunctionLSTM) == id){
-                 FunctionLSTM *fl =  (FunctionLSTM *)f;
 
-                FunctionLinear *x_i = (FunctionLinear *)fl->x_i;
-                FunctionLinear *w_i = (FunctionLinear *)fl->w_i;
 
-                FunctionLinear *x_f = (FunctionLinear *)fl->x_f;
-                FunctionLinear *w_f = (FunctionLinear *)fl->w_f;
 
-                FunctionLinear *x_o = (FunctionLinear *)fl->x_o;
-                FunctionLinear *w_o = (FunctionLinear *)fl->w_o;
-
-                FunctionLinear *x_g = (FunctionLinear *)fl->x_g;
-                FunctionLinear *w_g = (FunctionLinear *)fl->w_g;
-                x_i->toHostArray();
-                w_i->toHostArray();
-                x_f->toHostArray();
-                w_f->toHostArray();
-                x_o->toHostArray();
-                w_o->toHostArray();
-                x_g->toHostArray();
-                w_g->toHostArray();
-            }
         }
 
         std::ofstream ofs(path);
         boost::archive::binary_oarchive oa(ofs);
-        oa.register_type<FunctionLinear>(); // add if you define new function
-        oa.register_type<FunctionGRU>(); // add if you define new function
-        oa.register_type<FunctionLSTM>(); // add if you define new function
-        oa.register_type<FunctionEmbed>(); // add if you define new function
+        oa.register_type<Linear>(); // add if you define new function
+        oa.register_type<LSTM>(); // add if you define new function
+        oa.register_type<FullLSTM>(); // add if you define new function
+        oa.register_type<FullLSTM2>(); // add if you define new function
+        oa.register_type<GRU>(); // add if you define new function
+        oa.register_type<Tanh>(); // add if you define new function
+        oa.register_type<ReLU>(); // add if you define new function
+        oa.register_type<Dropout>(); // add if you define new function
+        oa.register_type<SoftmaxCrossEntropy>(); // add if you define new function
+        oa.register_type<Softmax>(); // add if you define new function
+        oa.register_type<MeanSquaredError>(); // add if you define new function
+        oa.register_type<Plus>(); // add if you define new function
+        oa.register_type<BatchNorm>(); // add if you define new function
+        oa.register_type<Conv2D>(); // add if you define new function
+        oa.register_type<Pooling>(); // add if you define new function
 
         oa << *this;
 
@@ -232,69 +215,76 @@ public:
     void load(string path){
         std::ifstream ifs(path);
         boost::archive::binary_iarchive ia(ifs);
-        ia.register_type<FunctionLinear>(); // add if you define new function
-        ia.register_type<FunctionGRU>(); // add if you define new function
-        ia.register_type<FunctionLSTM>(); // add if you define new function
-        ia.register_type<FunctionEmbed>(); // add if you define new function
+        ia.register_type<Linear>(); // add if you define new function
+        ia.register_type<LSTM>(); // add if you define new function
+        ia.register_type<FullLSTM>(); // add if you define new function
+        ia.register_type<FullLSTM2>(); // add if you define new function
+        ia.register_type<GRU>(); // add if you define new function
+        ia.register_type<Tanh>(); // add if you define new function
+        ia.register_type<ReLU>(); // add if you define new function
+        ia.register_type<Dropout>(); // add if you define new function
+        ia.register_type<SoftmaxCrossEntropy>(); // add if you define new function
+        ia.register_type<Softmax>(); // add if you define new function
+        ia.register_type<MeanSquaredError>(); // add if you define new function
+        ia.register_type<Plus>(); // add if you define new function
+        ia.register_type<BatchNorm>(); // add if you define new function
+        ia.register_type<Conv2D>(); // add if you define new function
+        ia.register_type<Pooling>(); // add if you define new function
+
 
         ia >> *this;
 
         ifs.close();
 
-        for(auto fs : funcs){
-            Function *f = fs.second;
+        for(auto gs : graphs){
 
-            const type_info& id = typeid(*f);
+            Graph *g = gs.second;
 
-            if (typeid(FunctionLinear) == id){
-                ((FunctionLinear *)f)->fromHostArray();
+            const type_info& id = typeid(*g);
+
+            if (typeid(Linear) == id){
+                ((Linear *)g)->fromHostArray();
             }
-            else if (typeid(FunctionEmbed) == id){
-                ((FunctionEmbed *)f)->fromHostArray();
+            else if (typeid(LSTM) == id){
+                ((LSTM *)g)->fromHostArray();
             }
-            else if (typeid(FunctionGRU) == id) {
-                FunctionGRU *fg = (FunctionGRU *) f;
-                FunctionLinear *u_h = (FunctionLinear *) fg->u_h;
-                FunctionLinear *ur_h = (FunctionLinear *) fg->ur_h;
-                FunctionLinear *uz_h = (FunctionLinear *) fg->uz_h;
-                FunctionLinear *w_x = (FunctionLinear *) fg->w_x;
-                FunctionLinear *wr_x = (FunctionLinear *) fg->wr_x;
-                FunctionLinear *wz_x = (FunctionLinear *) fg->wz_x;
-                u_h->fromHostArray();
-                ur_h->fromHostArray();
-                uz_h->fromHostArray();
-                w_x->fromHostArray();
-                wr_x->fromHostArray();
-                wz_x->fromHostArray();
+            else if (typeid(FullLSTM) == id){
+                ((FullLSTM *)g)->fromHostArray();
             }
-            else if (typeid(FunctionLSTM) == id){
-                 FunctionLSTM *fl =  (FunctionLSTM *)f;
-
-                FunctionLinear *x_i = (FunctionLinear *)fl->x_i;
-                FunctionLinear *w_i = (FunctionLinear *)fl->w_i;
-
-                FunctionLinear *x_f = (FunctionLinear *)fl->x_f;
-                FunctionLinear *w_f = (FunctionLinear *)fl->w_f;
-
-                FunctionLinear *x_o = (FunctionLinear *)fl->x_o;
-                FunctionLinear *w_o = (FunctionLinear *)fl->w_o;
-
-                FunctionLinear *x_g = (FunctionLinear *)fl->x_g;
-                FunctionLinear *w_g = (FunctionLinear *)fl->w_g;
-                x_i->fromHostArray();
-                w_i->fromHostArray();
-                x_f->fromHostArray();
-                w_f->fromHostArray();
-                x_o->fromHostArray();
-                w_o->fromHostArray();
-                x_g->fromHostArray();
-                w_g->fromHostArray();
+            else if (typeid(FullLSTM2) == id){
+                ((FullLSTM2 *)g)->fromHostArray();
+            }
+            else if (typeid(GRU) == id){
+                ((GRU *)g)->fromHostArray();
+            }
+            else if (typeid(BatchNorm) == id){
+                ((BatchNorm *)g)->fromHostArray();
+            }
+            else if (typeid(Conv2D) == id){
+                ((Conv2D *)g)->fromHostArray();
             }
         }
 
         getUpdateParams();
     }
 
+    void unchain(){
+        for(auto gs : graphs) {
+
+            Graph *g = gs.second;
+
+            g->remove_chain();
+        }
+    }
+
+    void zero_grads(){
+        for(auto gs : graphs) {
+
+            Graph *g = gs.second;
+
+            g->zero_grads();
+        }
+    }
 };
 
 #endif /* FUNCTION_SET_H_ */
